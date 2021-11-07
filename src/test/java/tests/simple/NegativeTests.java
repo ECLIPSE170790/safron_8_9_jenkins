6aad526feaa5d121a5e066a052bb05f63be45ae3
ECLIPSE170790 <eclipse170790@mail.ru>
2021-11-07 15:01:09 +0600

    added simple tests

---
+++ src/test/java/tests/simple/NegativeTests.java
@@ -0,0 +1,29 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+    @Test
+    void test0() {
+        assertTrue(false);
+    }
+
+    void test1() {
+        assertTrue(false);
+    }
+
+    void test2() {
+        assertTrue(false);
+    }
+
+    void test3() {
+        assertTrue(false);
+    }
+
+    void test4() {
+        assertTrue(false);
+    }
+
+}
---
+++ src/test/java/tests/simple/PositiveTests.java
@@ -0,0 +1,48 @@
+package tests.simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+    @Test
+    void test0() {
+        assertTrue(true);
+    }
+
+    void test1() {
+        assertTrue(true);
+    }
+
+    void test2() {
+        assertTrue(true);
+    }
+
+    void test3() {
+        assertTrue(true);
+    }
+
+    void test4() {
+        assertTrue(true);
+    }
+
+    void test5() {
+        assertTrue(true);
+    }
+
+    void test6() {
+        assertTrue(true);
+    }
+
+    void test7() {
+        assertTrue(true);
+    }
+
+    void test8() {
+        assertTrue(true);
+    }
+
+    void test9() {
+        assertTrue(true);
+    }
+}
