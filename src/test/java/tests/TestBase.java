cf8a1ac4a1596ce40c56aedc8a1b8ac1e4623626
ECLIPSE170790 <eclipse170790@mail.ru>
2021-11-28 19:40:25 +0600

    updated build.gradle

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         capabilities.setCapability("enableVideo", true);
 
         Configuration.browserCapabilities = capabilities;
-        //Configuration.startMaximized = true;
+        Configuration.startMaximized = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
